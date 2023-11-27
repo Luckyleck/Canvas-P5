05114ac46274f72ee06f78968f9def147803bcde
Alex <luckyleck29@gmail.com>
2023-11-27 12:01:25 -0500

    Canvas Ready

---
+++ Runner Game/script.js
@@ -0,0 +1,10 @@
+const canvas = document.createElement('canvas');
+document.body.appendChild(canvas)
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const c = canvas.getContext('2d');
+
+
+
