fdca9c2740362a4f89bb89b397d35a120a6b91bf
Alex <luckyleck29@gmail.com>
2023-12-21 21:52:51 -0500

    Pather Set up

--- Pather/script.js
+++ Pather/script.js
@@ -1,1 +1,36 @@
-cos
+const canvas = document.getElementById('canvas');
+canvas.style.backgroundColor = '#D59B9B'
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const c = canvas.getContext('2d');
+
+const squareSize = 50
+const circleArgs = [5, 0, 2 * Math.PI]
+
+function draw() {
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+
+    c.fillRect(centerX, centerY, squareSize, squareSize);
+
+    c.beginPath();
+    c.arc(centerX, centerY, ...circleArgs);
+    c.fillStyle = 'blue';
+    c.fill();
+    c.closePath();
+}
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    draw();
+}
+
+window.addEventListener('resize', resizeCanvas);
+
+draw();
+
+
